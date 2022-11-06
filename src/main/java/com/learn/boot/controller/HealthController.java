edc13caf62c33c8e3094aa5f449367b1b54fbf4f
baql <qinglai@163.com>
2022-11-06 19:06:18 +0800

    新文件第一次提交

---
+++ src/main/java/com/learn/boot/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.learn.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @RequestMapping("/health")
+    public String Handle01() {
+        System.out.println("第一次提交");
+        return "OK";
+    }
+}
