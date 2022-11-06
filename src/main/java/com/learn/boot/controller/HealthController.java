8c5cc2675dc1895bb794d76fbe2409558686d285
baql <qinglai@163.com>
2022-11-06 19:07:49 +0800

    新文件第二次提交

--- src/main/java/com/learn/boot/controller/HealthController.java
+++ src/main/java/com/learn/boot/controller/HealthController.java
@@ -9,6 +9,7 @@ public class HealthController {
     @RequestMapping("/health")
     public String Handle01() {
         System.out.println("第一次提交");
+        System.out.println("第二次提交");
         return "OK";
     }
 }
