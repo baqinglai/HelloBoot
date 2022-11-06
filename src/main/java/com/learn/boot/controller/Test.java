cd25fe61e0ce34711d5f4913c6d65af46f945080
baql <qinglai@163.com>
2022-11-06 19:46:19 +0800

    hot-fix 提交

---
+++ src/main/java/com/learn/boot/controller/Test.java
@@ -0,0 +1,11 @@
+package com.learn.boot.controller;
+
+/**
+ * @author baqinglai
+ * @date 2022/11/6
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hot-fix create");
+    }
+}
