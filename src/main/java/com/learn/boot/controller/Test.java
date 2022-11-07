6c54bb41507bb83dd8967f93442621b7338f5fab
baql <qinglai@163.com>
2022-11-07 19:48:49 +0800

    master 提交2

--- src/main/java/com/learn/boot/controller/Test.java
+++ src/main/java/com/learn/boot/controller/Test.java
@@ -6,8 +6,8 @@ package com.learn.boot.controller;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hot-fix create");
-        System.out.println("master 冲突测试");
-        System.out.println("hot-fix 冲突测试");
+        System.out.println("GitHub 测试");
+        System.out.println("GitHub 测试");
+        System.out.println("GitHub 测试");
     }
 }
