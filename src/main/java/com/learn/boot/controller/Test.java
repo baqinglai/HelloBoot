4a695180e4a3a41e548d9316e7a2ae4495d1898e
baql <qinglai@163.com>
2022-11-07 22:02:18 +0800

    GitHub 第一次提交

--- src/main/java/com/learn/boot/controller/Test.java
+++ src/main/java/com/learn/boot/controller/Test.java
@@ -8,6 +8,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("GitHub 测试");
         System.out.println("GitHub 测试");
-        System.out.println("GitHub 测试");
+        System.out.println("本地 测试");
+        System.out.println("本地 测试");
     }
 }
